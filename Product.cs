5aad58b9756f989851cbe0c0b3c8dcf0ac1e7eea
Ferney Chaparro <williamchaparro@unisangil.edu.co>
2023-09-18 10:07:58 -0500

    Create Product.cs

---
+++ Product.cs
@@ -0,0 +1,13 @@
+public class Product
+{
+    public string Name { get; set; }
+    public decimal Price { get; set; }
+    public int Stock { get; set; }
+
+    public Product(string name, decimal price, int stock)
+    {
+        Name = name;
+        Price = price;
+        Stock = stock;
+    }
+}
